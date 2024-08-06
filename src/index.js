fbc873da649a1f7e30bc908c1a9e669cf8fbe168
Josef F. <145183720+flarack@users.noreply.github.com>
2024-08-06 09:02:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1024,
-    height: 768,
+    height: 900,
     webPreferences: {
       devTools: false
     }
